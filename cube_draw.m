f9fc10725ea2c8f239a1c89b72befabe173728b7
Jens Petit <Jens Petit>
2016-05-17 14:44:26 +0900

    3D Reference plot of cube

---
+++ cube_draw.m
@@ -0,0 +1,16 @@
+fill3(cube(1,1:1:4),cube(2,1:1:4),cube(3,1:1:4),'m');
+hold on;
+fill3(cube(1,5:1:8),cube(2,5:1:8),cube(3,5:1:8),'m');
+
+fill3(cube(1,9:1:12),cube(2,9:1:12),cube(3,9:1:12),'m');
+
+fill3(cube(1,13:1:16),cube(2,13:1:16),cube(3,13:1:16),'m');
+
+fill3(cube(1,16:1:20),cube(2,16:1:20),cube(3,16:1:20),'m');
+
+fill3(cube(1,21:1:24),cube(2,21:1:24),cube(3,21:1:24),'m');
+
+axis([-2 2 -1 3 -1 6])
+axis square
+drawnow
+hold off
