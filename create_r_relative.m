0b656a17eb47401c5bd27691dd0a00d84f0a177e
Jens Petit <Jens Petit>
2016-05-17 16:15:34 +0900

    update plot

--- create_r_relative.m
+++ create_r_relative.m
@@ -44,11 +44,11 @@ r_rel_parabol(2,:) = linspace(-1,1,N);
 r_rel_parabol(1,:) = (r_rel_parabol(2,:).^2)*(-1);
 
 % cube shape
-r_rel_cube = [[0.5 0.5 -0.5]' [0.5 -0.5 -0.5]' [-0.5 0.5 -0.5]' [-0.5 -0.5 -0.5]' [0.5 0.5 0.5]' [0.5 -0.5 0.5]' [-0.5 0.5 0.5]' [-0.5 -0.5 0.5]'];
+r_rel_cube = [[0.4 0.4 -0.4]' [0.4 -0.4 -0.4]' [-0.4 0.4 -0.4]' [-0.4 -0.4 -0.4]' [0.4 0.4 0.4]' [0.4 -0.4 0.4]' [-0.4 0.4 0.4]' [-0.4 -0.4 0.4]'];
 
-r_rel_1 = r_rel_line;
+r_rel_3 = r_rel_line;
 r_rel_2 = r_rel_parabol;
-r_rel_3 = r_rel_cube;
+r_rel_1 = r_rel_cube;
 
 
 % plot of formations
--- plot_animated_traj_DesAndActual_pos.m
+++ plot_animated_traj_DesAndActual_pos.m
@@ -7,37 +7,51 @@ N = size(simout1.data,2);
 % number of timesteps
 timesteps = size(simout1.data,3);
 
+% cube setup
+cube_setup;
+
 figure('Position',[0 100 800 800]);
+
 
 % color vector
 c = ['b','m','k','r','g','b','m','k','r','g','b','m','k','r','g','b','m',...
     'k','r','g','b','m','k','r','g','b','m','k','r','g','b','m','k','r',...
     'g','b','m','k','r','g','b','m','k','r','g','b','m','k','r','g'];
 
-v = VideoWriter('C:\Users\Jens\Documents\Uni Stuttgart\BA\BA ohne Video\Matlab\Consensus_wheeled_formation\4 formation_control\Videos\vehicle_trajectories_changed4.mp4', 'MPEG-4');
+v = VideoWriter('C:\Users\Jens\Documents\Uni Stuttgart\BA\BA ohne Video\Matlab\Consensus_3D\1 formation_control\Videos\vehicle_trajectories.mp4', 'MPEG-4');
 v.FrameRate = 60;
 open(v);
+opengl('software')
 
 % loop over each data entry of the simout results
-for j=1:5:(timesteps)
+for j=1:30:(timesteps)
     % plot of the reference frame trajectory
-    plot(xi_ref.data(:,1),xi_ref.data(:,2));
-    axis([-2 2 -1 3])
+    plot3(xi_ref.data(:,1),xi_ref.data(:,2),xi_ref.data(:,3));
+    axis([-2 2 -1 3 -1 3]);
     hold on;
     
+    cube = desired_pos_abs(xi_ref.data(j,:)',cube_init);
+    fill3(cube(1,1:1:4),cube(2,1:1:4),cube(3,1:1:4),'m');
+    fill3(cube(1,5:1:8),cube(2,5:1:8),cube(3,5:1:8),'m');
+    fill3(cube(1,9:1:12),cube(2,9:1:12),cube(3,9:1:12),'m');
+    fill3(cube(1,13:1:16),cube(2,13:1:16),cube(3,13:1:16),'m');
+    fill3(cube(1,16:1:20),cube(2,16:1:20),cube(3,16:1:20),'m');
+    fill3(cube(1,21:1:24),cube(2,21:1:24),cube(3,21:1:24),'m');
+
     % loop over each vehicle
     for i=1:1:N
         % plots the x,y-values of the actual i-th robot position
         x = simout1.data(1,i,j);
         y = simout1.data(2,i,j);
-        plot(x,y,'d','Color',c(i));
+        z = simout1.data(3,i,j);
+        scatter3(x,y,z,'d','filled');
         
-        % plots i-th robot desired position
-        %         x_ref = simout.data(1,i,j);
-        %         y_ref = simout.data(2,i,j);
-        %         plot(x_ref, y_ref,'o', 'Color',c(i));
+        %plots i-th robot desired position
+        x_ref = simout.data(1,i,j);
+        y_ref = simout.data(2,i,j);
+        z_ref = simout.data(3,i,j);
+        scatter3(x_ref, y_ref, z_ref,'filled','o');
         
-        axis([-2 2 -1 3]);
     end
     writeVideo(v,getframe);
     %     legend('ref','v1','v1_des','v2','v2_des','v3','v3_des','v4','v4_des');
